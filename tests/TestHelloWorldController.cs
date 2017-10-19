aef30da4502ce86df627a344b75b3afcc1524afe
0xecafdeef <0xecafdeef@gmail.com>
2017-10-19 09:50:11 -0500

    Adding a test for the HelloWorldController

---
+++ tests/TestHelloWorldController.cs
@@ -0,0 +1,16 @@
+using api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace tests
+{
+    public class TestHelloWorldController
+    {
+        [Fact]
+        public void TestHelloResponseFromApi()
+        {
+            HelloWorldController controller = new HelloWorldController();
+            var result = Assert.IsType<OkResult>(controller.Get());
+        }
+    }
+}
