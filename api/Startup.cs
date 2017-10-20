8e7e72c3517913a9804e7dd871df1305714a31fc
0xecafdeef <0xecafdeef@gmail.com>
2017-10-20 10:52:53 -0500

    Add the swagger doc to the WebApi

--- api/Startup.cs
+++ api/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -8,11 +9,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace api
 {
     public class Startup
     {
+        private static string VERSION = "v0.0.1";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -24,11 +28,32 @@ namespace api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(VERSION, new Info
+                {
+                    Title = "Key-Shadowing API",
+                    Version = $"Alpha-{VERSION}",
+                    Description = "RESTful API for Hello Greetings",
+                    Contact = new Contact
+                    {
+                        Name = "Isaiah Griego",
+                        Email = "0xecafdeef@gmail.com",
+                        Url = "https://github.com/0xecafdeef/hello.git"
+                    }
+                }
+                );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/{VERSION}/swagger.json", $"Hello API{VERSION}");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
