cf8d87b07060fa21dcc55b78fbb4e317ecbaf89c
0xecafdeef <0xecafdeef@gmail.com>
2017-10-20 11:06:17 -0500

    Added another test to test against the message itself that it receives

--- tests/TestHelloWorldController.cs
+++ tests/TestHelloWorldController.cs
@@ -12,5 +12,12 @@ namespace tests
             HelloWorldController controller = new HelloWorldController();
             var result = Assert.IsType<ObjectResult>(controller.Get());
         }
+        [Fact]
+        public void TestHelloWorldControllerFromApiResponseMessage()
+        {
+            HelloWorldController controller = new HelloWorldController();
+            var result = Assert.IsType<ObjectResult>(controller.Get());
+            Assert.Equal("Hello World", result.Value);
+        }
     }
 }
