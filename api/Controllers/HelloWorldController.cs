b89d6b32e151699d28a8f9ad6af0b1993db9093e
0xecafdeef <0xecafdeef@gmail.com>
2017-10-20 10:55:48 -0500

    Added some XML doc for better documentation from the swagger component

--- api/Controllers/HelloWorldController.cs
+++ api/Controllers/HelloWorldController.cs
@@ -3,9 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
+    /// <summary>
+    /// A Controller to handle requests and greet requestors in kind
+    /// </summary>
     [Route("api/[Controller]")]
     public class HelloWorldController : Controller
     {
+        /// <summary>
+        /// Get a friendly greeting via Http GET
+        /// </summary>
+        /// <response code="200">On sucessul greeting </response>
         [HttpGet]
         public ActionResult Get()
         {
